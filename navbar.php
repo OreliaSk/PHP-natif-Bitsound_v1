4b155754cf8ceda07bd116eedd8fd07ef25ed98c
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-03-08 12:25:18 +0100

    creation d'un fichier navbar.php à inclure sur toutes les pages

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark px-5">
+	<div id="rotating-item-wrapper">
+	<img class="rotating-item" src="img/home/navbar/tt.png" alt="signature" />
+	<img class="rotating-item" src="img/home/navbar/ss.png" alt="signature" />
+	<img class="rotating-item" src="img/home/navbar/cc.png" alt="signature" />
+	</div>
+  <a class="navbar-brand" href="#">BITSOUND.</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link nav-site active mx-1" href="#">Accueil<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link mx-1" href="posts.php">Articles</a>
+      <a class="nav-item nav-link mx-1" href="artists.php">Artistes</a>
+      <a class="nav-item nav-link mx-1" href="festivals.php">Festivals</a>
+    </div>
+  </div>
+</nav>
